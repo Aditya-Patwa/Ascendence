289f4e0704f8bc2015b611bfa4f2d6e9e313f577
Aditya Patwa <adityapatwa.tech@gmail.com>
2025-01-26 22:43:57 +0530

    Gradient and Chart Added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Hero() {
     return (
-        <main>
-            <div className="w-screen grid py-12 md:py-8 px-4 lg:px-8">
+        <main className="w-screen overflow-x-hidden">
+            <div className="w-screen grid py-12 md:py-8 px-4 lg:px-8 relative">
                 <div className="max-w-7xl">
                     <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-9xl leading-none tracking-tight font-extrabold">
                         Empowering you to <span className="bg-clip-text text-transparent bg-gradient-to-tr from-blue-500 via-fuchsia-500 to-pink-500 font-black">ascend</span> beyond financial limits.
@@ -26,7 +26,17 @@ export default function Hero() {
                         </Link>
                     </div>
                 </div>
+
+                <div className="absolute -right-1/2 -bottom-1/2 -md:right-1/3 h-[500px] w-[500px] md:h-[1000px] md:w-[1000px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(79,70,229,.2),rgba(255,255,255,0))] -z-10"></div>
             </div>
+
+            <div className="grid place-content-center py-4 md:py-8 px-4 md:px-8 md:mt-4 lg:mt-8">
+                <div className="max-w-6xl rounded-3xl border-4 md:border-8 border-indigo-600 shadow-3xl overflow-hidden">
+                    <img src="Grow Your Money.png" />
+                </div>
+            </div>
+
+            
         </main>
     )
 }
