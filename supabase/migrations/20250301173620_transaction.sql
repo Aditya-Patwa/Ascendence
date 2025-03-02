c2a88a086378a7bf619c2accb1c98d35dcf1f807
Aditya Patwa <adityapatwa.tech@gmail.com>
2025-03-02 19:23:25 +0530

    Editted Transaction Category

--- supabase/migrations/20250301173620_transaction.sql
+++ supabase/migrations/20250301173620_transaction.sql
@@ -1,7 +1,7 @@
 create table transactions (
     id uuid primary key default gen_random_uuid(),
     user_profile uuid not null references public.profiles (id) default auth.uid() on delete cascade,
-    category text check (category in ('Food', 'Bills', 'Entertainment', 'Investment', 'Other')),
+    category text not null,
     amount numeric not null,
     transaction_type text check (transaction_type in ('Expense', 'Income')) not null,
     payment_method text check (payment_method in ('Card', 'UPI', 'Crypto', 'Cash')),
